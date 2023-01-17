347d1a8550a1df344fcf71a676f574f0c7cc7c14
ihtzmiimark <mark.bonggol@lsu.edu.ph>
2023-01-17 22:40:23 +0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div>
+            <logo />
+            <h1 class="Title">About</h1>
+            <div>
+                Hi miss!
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
